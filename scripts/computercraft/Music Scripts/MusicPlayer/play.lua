2a797b66e6398f2044f213af7d12e0eca41b39bf
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2024-10-27 13:04:50 -0400

    Update play.lua

--- scripts/computercraft/Music Scripts/MusicPlayer/play.lua
+++ scripts/computercraft/Music Scripts/MusicPlayer/play.lua
@@ -93,7 +93,7 @@ function playChunk(chunk)
 end
 
 if drive == nil or not drive.isDiskPresent() then
-	print("Playing at volume " .. (volume or 1.0))
+	print("Playing '" .. selectedSong .. "' at volume " .. (volume or 1.0))
 else
 	print("Playing '" .. drive.getDiskLabel() .. "' at volume " .. (volume or 1.0))
 end
