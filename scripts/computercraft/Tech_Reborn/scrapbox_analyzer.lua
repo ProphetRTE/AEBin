d0b25ee47646f1fb7adaa546a1c982ac9672ba84
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2024-10-29 12:06:29 -0400

    Update scrapbox_analyzer.lua

--- scripts/computercraft/Tech_Reborn/scrapbox_analyzer.lua
+++ scripts/computercraft/Tech_Reborn/scrapbox_analyzer.lua
@@ -15,11 +15,10 @@ while true do
         return
     end
     for k, v in pairs(items) do
-        local itemname = v.name:match(":(.*)$")
         if v.name == "techreborn:scrap_box" then
         else
-            local itemname = v.name:match(":(.*)$")
-            print(itemname)
+            local item = scrapinator.getItemDetail(2)
+            print(item.displayName)
             SRItems()
         end
     end
