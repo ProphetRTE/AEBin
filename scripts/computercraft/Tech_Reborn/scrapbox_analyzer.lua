682859998e7b3bcacf1e58b737e909dc862d7de2
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2024-10-29 11:46:09 -0400

    Update scrapbox_analyzer.lua

--- scripts/computercraft/Tech_Reborn/scrapbox_analyzer.lua
+++ scripts/computercraft/Tech_Reborn/scrapbox_analyzer.lua
@@ -2,14 +2,23 @@ local scrapperSide = "bottom"
 local scrapinator = peripheral.wrap(scrapperSide)
 local items = nil
 
+function SRItems()
+    redstone.setOutput(scrapperSide, true)
+    sleep(1)
+    redstone.setOutput(scrapperSide, false)
+end
+
 while true do
     local items = scrapinator.list()
-    if items == nil then return end
+    if items == nil then
+        SRItems()
+        return
+    end
     for k, v in pairs(items) do
         if v.name == "techreborn:scrap_box" then
         else
             print(v.name)
+            SRItems()
         end
     end
-    sleep(0.001)
 end
