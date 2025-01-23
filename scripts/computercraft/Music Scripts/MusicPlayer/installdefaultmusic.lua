19f5ecfa57a2acebef1958e2105e0e3ddc4d5446
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-01-23 14:48:12 -0500

    Update installdefaultmusic.lua

--- scripts/computercraft/Music Scripts/MusicPlayer/installdefaultmusic.lua
+++ scripts/computercraft/Music Scripts/MusicPlayer/installdefaultmusic.lua
@@ -18,7 +18,7 @@ local songNames = {
 -- Function to execute the command to save a song
 local function saveToDevice(songName, songUrl)
     local formattedName = songName:gsub("%%20", " ")  -- Decode %20 to spaces
-    local command = "savetodevice " .. "\"" .. formattedName .."\" " ..  " \"" .. songUrl .. "\""
+    local command = "savetodevice " .. "\"" .. formattedName .."\" " .. songUrl  -- Construct the command
     shell.run(command)  -- Execute the command
 end
 
