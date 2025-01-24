e40c7124af66976a0b797cb9694eeb0879174fcd
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-01-24 17:23:23 -0500

    added audio converter

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.svelte",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+  ],
+};
