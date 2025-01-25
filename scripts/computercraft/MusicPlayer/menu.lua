b14bb0a3e809c6a19593910e626fd93b010bf0e8
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-01-24 20:54:57 -0500

    Update menu.lua

--- scripts/computercraft/MusicPlayer/menu.lua
+++ scripts/computercraft/MusicPlayer/menu.lua
@@ -1,4 +1,7 @@
 local w, h = term.getSize()
+local entriesPerPage = 5
+local totalEntries = 0
+local currentPage = 1
 local selectedEntry = 1
 
 local menus = {}
@@ -10,11 +13,16 @@ function render()
     term.setCursorPos(1, 1)
 
     local menu = menus[currentMenu] or menus["main"]
+    totalEntries = #menu.entries
 
-    if selectedEntry > #menu.entries then
-        selectedEntry = 1
+    -- Adjust selected entry based on current page
+    local pageOffset = (currentPage - 1) * entriesPerPage
+    local maxPages = math.ceil(totalEntries / entriesPerPage)
+
+    if selectedEntry > entriesPerPage then
+        selectedEntry = entriesPerPage
     elseif selectedEntry < 1 then
-        selectedEntry = #menu.entries
+        selectedEntry = 1
     end
 
     if currentMenu ~= "main" then
@@ -23,45 +31,68 @@ function render()
             callback = function()
                 currentMenu = menu.parent or "main"
                 selectedEntry = 1 -- reset selection when going back
+                currentPage = 1 -- reset page when going back
                 render()
             end
         })
     end
 
-    term.setTextColor(colors.white)
-    for i = 1, #menu.entries do
+    -- Render menu entries for the current page
+    for i = 1, entriesPerPage do
+        local entryIndex = pageOffset + i
+        if entryIndex > totalEntries then
+            break
+        end
+
         local caret = selectedEntry == i and ">> " or "   "
         term.setTextColor(selectedEntry == i and colors.magenta or colors.white)
 
         -- Center the text visually
-        local entryLine = caret .. menu.entries[i].label
+        local entryLine = caret .. menu.entries[entryIndex].label
         local padded = string.format("%-" .. (w - 3) .. "s", entryLine)
         print(padded)
     end
 
-    term.setTextColor(colors.white) -- reset color at the end
+    -- Page indicators
+    term.setTextColor(colors.white)
+    print(string.format("Page %d of %d", currentPage, maxPages))
 end
 
 function onKeyPress(key)
     local menu = menus[currentMenu] or menus["main"]
 
     local switch = (({
-        [keys.enter] = menu.entries[selectedEntry].callback or function() end,
+        [keys.enter] = menu.entries[(currentPage - 1) * entriesPerPage + selectedEntry].callback or function() end,
 
         [keys.up] = function()
             selectedEntry = selectedEntry - 1
+            if selectedEntry < 1 then
+                if currentPage > 1 then
+                    currentPage = currentPage - 1
+                    selectedEntry = entriesPerPage
+                else
+                    selectedEntry = 1
+                end
+            end
         end,
 
         [keys.down] = function()
             selectedEntry = selectedEntry + 1
+            if selectedEntry > entriesPerPage then
+                if currentPage < math.ceil(totalEntries / entriesPerPage) then
+                    currentPage = currentPage + 1
+                    selectedEntry = 1
+                else
+                    selectedEntry = entriesPerPage
+                end
+            end
         end
     })[key] or function() end)
 
     switch()
 
-    local totalEntries = #menu.entries
-    if selectedEntry > totalEntries or selectedEntry <= 0 then
-        selectedEntry = selectedEntry > totalEntries and totalEntries or 1
+    if selectedEntry > entriesPerPage or selectedEntry <= 0 then
+        selectedEntry = selectedEntry > entriesPerPage and entriesPerPage or 1
     end
 end
 
