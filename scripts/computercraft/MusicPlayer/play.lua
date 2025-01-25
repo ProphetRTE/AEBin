f224623e48c9d5bbbe39c1b2cb25af1320626d0a
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-01-25 09:33:20 -0500

    Update play.lua

--- scripts/computercraft/MusicPlayer/play.lua
+++ scripts/computercraft/MusicPlayer/play.lua
@@ -4,216 +4,155 @@ local drive = peripheral.find("drive")
 local decoder = dfpwm.make_decoder()
 
 local menu = require "menu"
-local math_random = math.random
-local songs = {}
+
 local uri = nil
 local volume = settings.get("media_center.volume")
 local selectedSong = nil
-local isShuffleEnabled = false
-local isPaused = false
-local quit = false
-local isLoopEnabled = false
 
 if drive == nil or not drive.isDiskPresent() then
-    local savedSongs = fs.list("songs/")
-    
-    if #savedSongs == 0 then
-        error("ERR - No disk was found in the drive, or no drive was found. No sound files were found saved to device.")
-    else
-        local entries = {
-            [1] = {
-                label = "[CANCEL]",
-                callback = function()
-                    error()
-                end
-            }
-        }
+	local savedSongs = fs.list("songs/")
 
-        for i, fp in ipairs(savedSongs) do
-            table.insert(entries, {
-                label = fp:match("^([^.]+)"),
-                callback = function()
-                    selectedSong = fp
-                    menu.exit()
-                end
-            })
-        end
+	if #savedSongs == 0 then
+		error("ERR - No disk was found in the drive, or no drive was found. No sound files were found saved to device.")
+	else
+		local entries = {
+			[1] = {
+				label = "[CANCEL]",
+				callback = function()
+					error()
+				end
+			}
+		}
 
-        menu.init({
-            main = {
-                entries = entries
-            }
-        })
+		for i, fp in ipairs(savedSongs) do
+			table.insert(entries, {
+				label = fp:match("^([^.]+)"),
+				callback = function()
+					selectedSong = fp
 
-        menu.thread()
+					menu.exit()
+				end
+			})
+		end
 
-        if selectedSong ~= nil then
-            local fp = "songs/" .. selectedSong
+		menu.init({
+			main = {
+				entries = entries
+			}
+		})
 
-            if fs.exists(fp) then
-                local file = fs.open(fp, "r")
-                uri = file.readAll()
-                file.close()
-            else
-                print("Song was not found on device!")
-                return
-            end
-        else error() end
-    end
+		menu.thread()
+
+		if selectedSong ~= nil then
+			local fp = "songs/" .. selectedSong
+
+			if fs.exists(fp) then
+				local file = fs.open(fp, "r")
+
+				uri = file.readAll()
+
+				file.close()
+			else
+				print("Song was not found on device!")
+
+				return
+			end
+		else error() end
+	end
 else
-    local songFile = fs.open("disk/song.txt", "r")
-    uri = songFile.readAll()
-    songFile.close()
+	local success, err = pcall(function()
+        local songFile = fs.open("disk/song.txt", "r")
+        uri = songFile.readAll()
+        songFile.close()
+    end)
+
+    if not success then
+        print("ERR - Unable to read disk: " .. err)
+        return nil
+    end
 end
 
 if uri == nil or not uri:find("^https") then
-    print("ERR - Invalid URI!")
-    return
+	print("ERR - Invalid URI!")
+	return
 end
 
 function playChunk(chunk)
-    local returnValue = nil
-    local callbacks = {}
+	local returnValue = nil
+	local callbacks = {}
 
-    for i, speaker in pairs(speakers) do
-        if i > 1 then
-            table.insert(callbacks, function()
-                speaker.playAudio(chunk, volume or 1.0)
-            end)
-        else
-            table.insert(callbacks, function()
-                returnValue = speaker.playAudio(chunk, volume or 1.0)
-            end)
-        end
-    end
+	for i, speaker in pairs(speakers) do
+		if i > 1 then
+			table.insert(callbacks, function()
+				speaker.playAudio(chunk, volume or 1.0)
+			end)
+		else
+			table.insert(callbacks, function()
+				returnValue = speaker.playAudio(chunk, volume or 1.0)
+			end)
+		end
+	end
 
-    parallel.waitForAll(table.unpack(callbacks))
+	parallel.waitForAll(table.unpack(callbacks))
 
-    return returnValue
+	return returnValue
 end
 
--- Function to shuffle the songs
-function shuffleSongs()
-    if #songs == 0 then return end
-    for i = #songs, 2, -1 do
-        local j = math_random(i)
-        songs[i], songs[j] = songs[j], songs[i]
-    end
-end
+print("Playing '" .. drive.getDiskLabel() .. "' at volume " .. (volume or 1.0))
 
-function updateURI()
-    if #songs == 0 then
-        print("No songs left to play. Reshuffling...")
-        songs = fs.list("songs/")
-        shuffleSongs() -- Reshuffle the songs
-    end
-
-    selectedSong = songs[1] -- Select the first song
-    uri = "songs/" .. selectedSong -- Update the URI
-end
-
+local quit = false
 
 function play()
-    while true do
-        
-        songs = fs.list("songs/")
-        if isShuffleEnabled then
-            shuffleSongs() -- Shuffle the songs
-        end
+	while true do
+		local response = http.get(uri, nil, true)
 
-        if not selectedSong then
-            print("No song selected to play.")
-            break -- Exit loop if there's no song selected
-        end
+		local chunkSize = 4 * 1024
+		local chunk = response.read(chunkSize)
+		while chunk ~= nil do
+			local buffer = decoder(chunk)
 
-        if not isLoopEnabled then
-            updateURI() -- Update the URI to the next song
-        end
+			while not playChunk(buffer) do
+				os.pullEvent("speaker_audio_empty")
+			end
 
-        
-
-        print("Now playing: " .. selectedSong)
-        local response = http.get(uri, nil, true)
-
-        local chunkSize = 4 * 1024
-        local chunk
-        while true do
-            if isPaused then
-                -- Wait for a signal to continue playing
-                os.pullEvent("resume")
-            end
-            
-            chunk = response.read(chunkSize)
-            if not chunk then
-                print("Song ended: " .. selectedSong)
-                break
-            end
-
-            local buffer = decoder(chunk)
-            while not playChunk(buffer) do
-                os.pullEvent("speaker_audio_empty")
-            end
-        end
-    end
+			chunk = response.read(chunkSize)
+		end
+	end
 end
 
 function readUserInput()
-    local commands = {
-        ["stop"] = function()
-            print("Stopping the media center.")
-            quit = true
-        end,
-        ["shuffle"] = function()
-            isShuffleEnabled = not isShuffleEnabled
-            print("Shuffle is now " .. (isShuffleEnabled and "enabled" or "disabled"))
-        end,
-        ["pause"] = function()
-            isPaused = not isPaused
-            if isPaused then
-                print("Media is paused. Type 'resume' to continue.")
-            else
-                print("Media is playing now.")
-                os.queueEvent("resume")  -- Trigger the resume event
-            end
-        end,
-        ["skip"] = function()
-            print("Skipping to the next song.")
-            updateURI() -- Update the URI to the next song
-        end,
-        ["loop"] = function()
-            isLoopEnabled = not isLoopEnabled
-            print("Loop is now " .. (isLoopEnabled and "enabled" or "disabled"))
-        end
-    }
+	local commands = {
+		["stop"] = function()
+			quit = true
+		end
+	}
 
-    while true do
-        local input = string.lower(read())
-        local commandName = ""
-        local cmdargs = {}
+	while true do
+		local input = string.lower(read())
+		local commandName = ""
+		local cmdargs = {}
 
-        local i = 1
-        for word in input:gmatch("%w+") do
-            if i > 1 then
-                table.insert(cmdargs, word)
-            else
-                commandName = word
-            end
-            i = i + 1
-        end
+		local i = 1
+		for word in input:gmatch("%w+") do
+			if i > 1 then
+				table.insert(cmdargs, word)
+			else
+				commandName = word
+			end
+		end
 
-        local command = commands[commandName]
-        if command then
-            command(table.unpack(cmdargs))
-        else
-            print('"' .. cmdargs[1] .. '" is not a valid command!')
-        end
-    end
+		local command = commands[commandName]
+
+		if command ~= nil then
+			command(table.unpack(cmdargs))
+		else print('"' .. cmdargs[1] .. '" is not a valid command!') end
+	end
 end
 
 function waitForQuit()
-    while not quit do
-        sleep(0.1)
-    end
+	while not quit do
+		sleep(0.1)
+	end
 end
 
 parallel.waitForAny(play, readUserInput, waitForQuit)
