cc8dceeff34882ca63ec97b63a13548816b9bc2f
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-01-24 20:51:05 -0500

    Update menu.lua

--- scripts/computercraft/MusicPlayer/menu.lua
+++ scripts/computercraft/MusicPlayer/menu.lua
@@ -6,68 +6,77 @@ local currentMenu = "main"
 local running = true
 
 function render()
-	term.clear()
-	term.setCursorPos(1, 1)
+    term.clear()
+    term.setCursorPos(1, 1)
 
-	local menu = menus[currentMenu] or menus["main"]
+    local menu = menus[currentMenu] or menus["main"]
 
-	if selectedEntry > #menu.entries then
-		selectedEntry = 1
-	end
+    if selectedEntry > #menu.entries then
+        selectedEntry = 1
+    elseif selectedEntry < 1 then
+        selectedEntry = #menu.entries
+    end
 
-	if currentMenu ~= "main" then
-		table.insert(menu.entries, {
-			label = "[BACK]",
-			callback = function()
-				currentMenu = menu.parent or "main"
+    if currentMenu ~= "main" then
+        table.insert(menu.entries, {
+            label = "[BACK]",
+            callback = function()
+                currentMenu = menu.parent or "main"
+                selectedEntry = 1 -- reset selection when going back
+                render()
+            end
+        })
+    end
 
-				render()
-			end
-		})
-	end
+    term.setTextColor(colors.white)
+    for i = 1, #menu.entries do
+        local caret = selectedEntry == i and ">> " or "   "
+        term.setTextColor(selectedEntry == i and colors.magenta or colors.white)
 
-	for i = 1, #menu.entries do
-		local caret = selectedEntry == i and ">> " or "   "
-		term.setTextColor(selectedEntry == i and colors.magenta or colors.white)
+        -- Center the text visually
+        local entryLine = caret .. menu.entries[i].label
+        local padded = string.format("%-" .. (w - 3) .. "s", entryLine)
+        print(padded)
+    end
 
-		print(caret .. menu.entries[i].label)
-	end
+    term.setTextColor(colors.white) -- reset color at the end
 end
 
 function onKeyPress(key)
-	local menu = menus[currentMenu] or menus["main"]
+    local menu = menus[currentMenu] or menus["main"]
 
-	local switch = (({
-		[keys.enter] = menu.entries[selectedEntry].callback or function() end,
+    local switch = (({
+        [keys.enter] = menu.entries[selectedEntry].callback or function() end,
 
-		[keys.up] = function()
-			selectedEntry = selectedEntry - 1
-		end,
+        [keys.up] = function()
+            selectedEntry = selectedEntry - 1
+        end,
 
-		[keys.down] = function()
-			selectedEntry = selectedEntry + 1
-		end
-	})[key] or function() end)
+        [keys.down] = function()
+            selectedEntry = selectedEntry + 1
+        end
+    })[key] or function() end)
 
-	switch()
+    switch()
 
-	if selectedEntry > #menu.entries or selectedEntry <= 0 then
-		selectedEntry = 1
-	end
+    local totalEntries = #menu.entries
+    if selectedEntry > totalEntries or selectedEntry <= 0 then
+        selectedEntry = selectedEntry > totalEntries and totalEntries or 1
+    end
 end
 
 function thread()
-	while running do
-		render()
+    while running do
+        render()
 
-		local event, key = os.pullEvent "key"
-		onKeyPress(key)
-	end
+        local event, key = os.pullEvent("key")
+        onKeyPress(key)
+    end
 end
 
 return {
-	init = function(m) menus = m end,
-	exit = function() running = false end,
-	render = render,
-	thread = thread
+    init = function(m) menus = m end,
+    exit = function() running = false end,
+    render = render,
+    thread = thread
 }
