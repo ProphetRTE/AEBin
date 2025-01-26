bc77b9ee8be00888cbdf69073a3d7e7e1eadd2a3
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-01-25 20:18:03 -0500

    Updated scripts

--- scripts/computercraft/aepkg/aepkginstall.lua
+++ scripts/computercraft/aepkg/aepkginstall.lua
@@ -124,6 +124,11 @@ if (args[1]=="install") or (args[1]==nil) then
 		return false
 	end
 	print("[Installer] Successfully installed 'aeprint'!")
+	print("[Installer] Installing 'aecord' library...")
+	if downloadfile("lib/aecord","https://cc.prophecypixel.com/scripts/computercraft/aecord/aecord.lua")== false then
+		return false
+	end
+	print("[Installer] Successfully installed 'aecord'!")
 	print("[Installer] Installing 'aepkg'...")
 	if downloadfile("aepkg","https://cc.prophecypixel.com/scripts/computercraft/aepkg/aepkg")==false then
 		return false
