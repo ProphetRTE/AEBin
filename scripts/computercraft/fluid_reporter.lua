23aad2b217fbfb971f6ce992564b1bcc5c4142da
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-02-02 06:53:46 -0500

    Update fluid_reporter.lua

--- scripts/computercraft/fluid_reporter.lua
+++ scripts/computercraft/fluid_reporter.lua
@@ -1,81 +1,24 @@
--- Require the aeprogress library
-os.loadAPI("lib/aeprogress")
-os.loadAPI("lib/aeprint")
+local drain = peripheral.wrap("tconstruct_smelterydrain_0")
 
--- Function to check if a peripheral has the required functions
-local function hasRequiredFunctions(peripheralName)
-    return peripheral.hasType(peripheralName, "tank") or peripheral.hasType(peripheralName, "fluid")
-end
+-- This is very important! From my testing calling drain.getTankInfo("unknown") will not work!
+-- We're going to find the side of the drain needed to get our info when calling getTankInfo and cache it.
 
--- Function to calculate the percentage of a tank
-local function calculatePercentage(tankSize, fluidAmount)
-    if tankSize == 0 then
-        return 0
-    else
-        return math.floor((fluidAmount / tankSize) * 100)
-    end
-end
-
--- Function to format the tank information
-local function formatTank(peripheralName, tankName, tankSize, fluidAmount)
-    local percentage = calculatePercentage(tankSize, fluidAmount)
-    local bar = ""
-    for i = 1, percentage do
-        bar = bar .. "="
-    end
-    for i = 1, 100 - percentage do
-        bar = bar .. "-"
-    end
-    
-    return {
-        name = peripheralName .. ": " .. tankName,
-        size = tankSize,
-        fullnessBar = bar,
-        percentage = percentage
-    }
-end
-
--- Function to process the tank peripherals
-local function processTank(peripheralName)
-    local fluidPeripheral = peripheral.wrap(peripheralName)
-    
-    -- Check for the required functions
-    if fluidPeripheral.pullFluid and fluidPeripheral.tanks and fluidPeripheral.pushFluid then
-        local tanks = fluidPeripheral.tanks()
-        local tankInfo = {}
-        
-        for _, tank in ipairs(tanks) do
-            -- Assume each tank has a `getFluid` method that returns the amount of fluid (this can vary by mod)
-            local fluidAmount = fluidPeripheral.getFluid(tank)
-            local tankDetails = formatTank(peripheralName, tank, fluidPeripheral.getTankInfo(tank).capacity, fluidAmount)
-
-            -- Store tank information for progress display
-            actionTable = {}
-            actionTable[1] = function()
-                aeprint.aeprint(tankDetails.name .. "\n  Size: " .. tankDetails.size .. "mB\n  Fullness: " .. tankDetails.fullnessBar .. " (" .. tankDetails.percentage .. "%)\n", 0)
-            end
-            -- Add entries to this table as necessary.
+local tankSide = (function()
+      for _, direction in ipairs({'top', 'bottom', 'right', 'left'}) do
+        local info = drain.getTankInfo(direction)
+        if info and info[1] and info[1].contents then
+          return direction -- we found the direction of the smeltery internal tanks relative to the drain...
+          -- this is saved in var 'tankSide' for later calls to getTankInfo
         end
-        
-        -- Display progress
-        aeprogress.aeprogress(actionTable, false)
-    else
-        aeprint.aeprint("Peripheral " .. peripheralName .. " does not support required functions.", 0)
-    end
-end
+      end
+    end)() -- this anonymous function is invoked immediately and returns the tankSide of interest
 
--- Initialize Rednet
-rednet.open("top")  -- Change "top" to your connected side (e.g., "left", "right", etc.)
+if not tankSide then error("Inner smeltery tanks couldn't be found! Is the smeltery drain attached??", 0) end
 
--- Get a list of all connected peripherals
-local peripherals = peripheral.getNames()
+-- unpack will return all the elements in an indexed table or array in order. multiple assignment.
+-- info = drain.getTankInfo(tankSide); tank1, tank2 = info[1], info[2], ... unpack works like so
 
--- Check each peripheral for required functions
-for _, peripheralName in ipairs(peripherals) do
-    if hasRequiredFunctions(peripheralName) then
-        processTank(peripheralName)
-    end
-end
+local tank1, tank2 = unpack( drain.getTankInfo(tankSide) )
+-- tank1 should be the molten contents and tank2 should be the smeltery fuel(lava usually i think...)
 
--- Close Rednet when done
-rednet.close("top")
+ print( "contents: ", textutils.serialize(tank1.contents) )
