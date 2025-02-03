567e2294397130f327bb9bd586c9e129fc76e445
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-02-03 06:19:30 -0500

    Update fluid_reporter.lua

--- scripts/computercraft/fluid_reporter.lua
+++ scripts/computercraft/fluid_reporter.lua
@@ -3,18 +3,16 @@ local utils = require("lib/aeutils")
 -- Valve, Monitor & Wireless Modem
 local val = peripheral.wrap("tconstruct:drain_0")
 local mon = peripheral.find("monitor")
-local wmod = peripheral.wrap(utils.resolveModemSide())
+local wmod = peripheral.wrap(utils.resolveModemSide(nil))
 
-local warning = 20       -- Warning level in %
 local cap                -- Tank capacity
 local amount             -- Amount liquid in tank
-local percentfull        -- Percent liquid in tank
-local lastpercent = 1000 -- Percent last loop
+local lastAmount = 0     -- Last amount in tank
 local sendmsg            -- Message to send
 local sleeptime          -- How long to sleep
 local sendFreq = 3       -- Modem Frequency
 local content = "Water"  -- What is in tank?
--- Make sure frequency match the main computer
+-- Make sure frequency matches the main computer
 
 -- Set warning lamp to off
 redstone.setOutput("right", false)
@@ -25,55 +23,51 @@ while true do
   mon.setCursorPos(1,1)
 
   -- Fill table with data from tank valve
-  tanksTable = val.getTanks("WhatIsThis")
+  tanksTable = val.tanks()
   maintank = tanksTable[1]
 
   -- Get values for tank capacity and amount
-  cap = maintank.capacity / 1000   -- in buckets
+  cap = maintank.amount / 1000   -- in buckets
   amount = maintank.amount    -- in millibuckets
   
   -- If tank is empty, to avoid math issues with 0
   if amount == nil then
     amount = 0
-    percentfull = 0
   else
     -- Use math.floor to convert to integers
     amount = math.floor(amount / 1000)
-    percentfull = math.floor(100 * amount / cap)
   end
 
   -- Self explanatory :)
   mon.write(content)
   mon.setCursorPos(1,2)
   mon.write("Amount: " ..amount .."/"..cap .." B.")
-  mon.setCursorPos(1,3)
-  mon.write("Amount: " ..percentfull .."%  ")
 
-  -- Check for change since last loop  
-  if percentfull == lastpercent then
-    print("Still " ..percentfull .. "%, nothing sent.")
+  -- Check for change since the last loop  
+  if amount == lastAmount then
+    print("Still " ..amount .. " B, nothing sent.")
   else
     -- If value changed, send to main!
-    sendmsg = content ..": " ..percentfull .." %"
+    sendmsg = content ..": " ..amount .." B"
     --wmod.transmit(sendFreq,0,sendmsg)
     print("Sent: " ..sendmsg)
   end
 
   -- Save for next loop
-  lastpercent = percentfull
+  lastAmount = amount
 
   -- Warning control, local lamp
   mon.setCursorPos(1,5)
   
-  if percentfull < warning then
+  if amount < 20 then  -- Warning level set at 20 B for example
     redstone.setOutput("right", true)
-    mon.write("Less than " ..warning .."% full")
+    mon.write("Less than 20 B full")
     sleep(1)
     redstone.setOutput("right", false)
     sleeptime = 1 
   else
     -- Above warning level, sleep longer
-    mon.write("More than " ..warning .."% full")
+    mon.write("More than 20 B full")
     sleeptime = 10
   end
 
