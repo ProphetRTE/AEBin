0fbb53f3654ff2ef4588eb51c5b31a7700c688f8
AEV1ridis <48230320+ProphetRTE@users.noreply.github.com>
2025-02-02 21:26:47 -0500

    updated aenet and fluid reporter to use library functions

--- scripts/computercraft/fluid_reporter.lua
+++ scripts/computercraft/fluid_reporter.lua
@@ -1,7 +1,9 @@
+local utils = require("lib/aeutils")
+
 -- Valve, Monitor & Wireless Modem
 local val = peripheral.wrap("tconstruct:drain_0")
 local mon = peripheral.find("monitor")
-local wmod = peripheral.wrap(resolveModemSide())
+local wmod = peripheral.wrap(utils.resolveModemSide())
 
 local warning = 20       -- Warning level in %
 local cap                -- Tank capacity
@@ -16,44 +18,6 @@ local content = "Water"  -- What is in tank?
 
 -- Set warning lamp to off
 redstone.setOutput("right", false)
-
-
--- Used to process modem side arguments passed to functions.
-function resolveModemSide(modemSide)
-	-- If no modem side argument is provided, search for a modem and use that side.
-  if modemSide == nil then
-    for _,side in pairs(peripheral.getNames()) do
-		if peripheral.getType(side) == "modem" then
-			local modem = peripheral.wrap(side)
-			if modem.isWireless() then
-				modemSide = side
-				break
-			end
-		end
-    end
-    if modemSide == nil then
-      error("Could not find a modem.", 3)
-    end
-  else
-		-- If an argument was provided, check that it is a valid side.
-    local found = false
-    for _,side in pairs(redstone.getSides()) do
-      if side == modemSide then
-        found = true
-        break
-      end
-    end
-    if not found then
-      error(tostring(modemSide).." is not a valid side.", 3)
-    end
-  end
-  if peripheral.getType(modemSide) ~= "modem" then
-    error("No modem on side "..modemSide..".", 3)
-  end
-
-  log("Using modem "..modemSide..".")
-  return modemSide
-end
 
 -- Main prog loop, never stop
 while true do
